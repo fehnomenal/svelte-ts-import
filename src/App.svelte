91e042f4858924246aa374ce9f4beedd1a3b1538
Andreas Fehn <fehnomenal@fehn.systems>
2024-11-20 23:30:13 +0100

    not working

--- src/App.svelte
+++ src/App.svelte
@@ -1,4 +1,7 @@
-<script lang="ts">
+<script
+  lang="ts"
+  generics="SomethingLongToTriggerReformat extends Record<string, unknown>, MoreStrings extends keyof SomethingLongToTriggerReformat"
+>
   import { test } from './lib/test.js';
 </script>
 
